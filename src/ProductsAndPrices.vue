8b3904b55d3407b7c98e2084d4a143fbea85b32f
Elena Moreva <lenka.moreva@gmail.com>
2023-09-01 11:43:52 +0300

    Реализована возможность добавлять продукты

--- src/ProductsAndPrices.vue
+++ src/ProductsAndPrices.vue
@@ -1,11 +1,103 @@
 <template>
-  <h1>2</h1>
+  <div class="text-h4" style="margin: 10px">Введите названия и цены продуктов</div>
+
+  <hr><br>
+
+
+  <div v-for="(product, ind) in this.$store.state.products" :key="ind">
+    <v-card
+      class="mx-auto"
+      max-width="450"
+      variant="outlined"
+      style="margin-bottom: 10px;">
+
+      <v-card-actions style=" border-bottom: #777;">
+        <h3 style="float: left; margin-right: 100px;">{{ product.name }} - {{ product.cost }}</h3>
+        <div style="float: right">
+          <v-btn variant="outlined" @click="this.$store.commit('delProduct', ind)">X</v-btn>
+          <v-btn variant="outlined">править</v-btn>
+        </div>
+
+      </v-card-actions>
+
+      <v-card-item>
+        <div>
+          <div style="float: left; margin-right: 15px;">
+            <p>кто платил?</p>
+
+            <v-radio-group v-for="(buyer, ind) in this.$store.state.buyers" :key="ind" v-model="product.buyer">
+              <v-radio  :label="buyer.name" :value="ind"></v-radio>
+            </v-radio-group>
+
+          </div>
+
+          <div >
+            <p>кто ел?</p>
+            <div v-for="(buyer, ind) in this.$store.state.buyers" :key="ind">
+              <v-checkbox  style="margin: 0; padding: 0;" :label="buyer.name" :value="ind" v-model="product.eaters"></v-checkbox>
+            </div>
+          </div>
+        </div>
+      </v-card-item>
+
+
+
+    </v-card>
+  </div>
+
+
+  <v-text-field @blur="this.$store.state.tempProduct.name = $event.target.value" v-model="this.$store.state.tempProduct.name">
+  </v-text-field>
+  <v-text-field @blur="this.$store.state.tempProduct.cost = $event.target.value" v-model="this.$store.state.tempProduct.cost">
+  </v-text-field>
+  <v-btn @click="this.$store.commit('addProduct')" style="width: 100%;" >+</v-btn>
+
+
+  {{ this.$store.state.products}}
+
+
 </template>
 
+
 <script>
+
+
 export default {
-  name: "ProductsAndPrices"
+  name: "ProductsAndPrices",
+  data: () => ({
+    model: null,
+    modell: null,
+    namess: 4,
+    arr:''
+  }),
+  methods: {
+    testProductName(){
+      if(this.$store.state.tempProduct.name){
+        return true
+      }
+    },
+    testProductCost(){
+      if(this.$store.state.tempProduct.cost){
+        return true
+      }
+    },
+    chooseBuyer(name){
+      return name
+    },
+    add(){
+      if(this.testProductName() && this.testProductCost()) {
+        this.$store.commit('addProduct')
+      }
+    },
+    getBuyerName(){
+      //return 'xui'
+      console.log(this.$store.state.buyers[1].name)
+      return String(this.$store.state.buyers[1].name)
+    }
+  }
 }
+
+
 </script>
 
 <style scoped>
--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,42 @@ import { createStore } from 'vuex'
 const store = createStore({
   state () {
     return {
+      testArray: [{name:'a'},
+        {name: 'b'}, {name: 'c'}],
+      arr: '',
       tempBuyerName:'',
-      buyers: []
+      buyers: [{name:'f'},
+        {name: 's'}, {name: 't'}],
+      tempProduct: {
+        name: '',
+        cost: ''
+      },
+      products: [],
     }
   },
   mutations: {
-    addBuyer(state, newName) {
-      state.buyers.push({name: newName})
+    addBuyer(state) {
+      state.buyers.push({name: state.tempBuyerName})
     },
     delBuyer(state, ind){
       state.buyers.splice(ind, 1)
-    }
+    },
+    addProduct(state) {
+      state.products.push({name: state.tempProduct.name,
+        cost: state.tempProduct.cost,
+        buyer: null,
+        eaters: []})
+      console.log(state.products)
+    },
+    delProduct(state, ind){
+      state.products.splice(ind, 1)
+    },
+    chooseBuyer(state, ind,  name){
+      state.products[ind].buyer = name
+    },
+    chooseEaters(state, ind, names){
+      state.products[ind].eaters = names
+    },
   }
 })
 
