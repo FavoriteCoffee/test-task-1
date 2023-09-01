1d2e37dd1a3616cc3455ecc81f0b5400117eb5a5
Elena Moreva <lenka.moreva@gmail.com>
2023-09-01 09:55:22 +0300

    Реализована возможность добавлять покупателей

--- src/BuyersNames.vue
+++ src/BuyersNames.vue
@@ -1,10 +1,48 @@
 <template>
-  <h1>1</h1>
+  <div class="text-h4" style="margin: 10px">Введите имена покупателей</div>
+
+  <hr><br>
+
+
+  <div v-for="(buyer, ind) in this.$store.state.buyers" :key="ind">
+    <v-card
+      class="mx-auto"
+      max-width="450"
+      variant="outlined"
+      style="margin-bottom: 10px;">
+
+      <v-card-item style="float: left">
+        <div class="text-h6 mb-1">{{ buyer.name }}</div>
+      </v-card-item>
+      <v-card-actions style="float: right">
+        <v-btn variant="outlined"  @click="this.$store.commit('delBuyer', ind)">
+          X
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+
+  <v-text-field @change="this.$store.state.tempBuyerName = $event.target.value">
+
+  </v-text-field>
+  <v-btn @click="this.$store.commit('addBuyer', this.$store.state.tempBuyerName)">+</v-btn>
+
 </template>
 
 <script>
+
 export default {
-  name: "BuyersNames"
+
+
+  methods: {
+    change(){
+      this.$store.commit('increment', 10)
+    },
+    add(val){
+      this.$store.commit('addBuyer', val)
+    }
+
+  }
 }
 </script>
 
