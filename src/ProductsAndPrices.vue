ff92d4a1bd755417c24d76b647b80e8f3b442831
Elena Moreva <lenka.moreva@gmail.com>
2023-09-03 13:57:02 +0300

    Внесены косметические изменения

--- src/ProductsAndPrices.vue
+++ src/ProductsAndPrices.vue
@@ -7,36 +7,33 @@
   <div v-for="(product, ind) in this.$store.state.products" :key="ind">
     <v-card
       class="mx-auto"
-      max-width="450"
+      max-width="500"
       variant="outlined"
       style="margin-bottom: 10px;">
 
-      <v-card-actions style=" border-bottom: #777;">
-        <h3 style="float: left; margin-right: 100px;">{{ product.name }} - {{ product.cost }}</h3>
-        <div style="float: right">
+      <v-card-actions style="float: right; border-bottom: #777; max-width: 200px;">
           <v-btn variant="outlined" @click="this.$store.commit('delProduct', ind)">X</v-btn>
           <v-btn variant="outlined">править</v-btn>
-        </div>
-
       </v-card-actions>
 
-      <v-card-item>
-        <div>
+      <v-card-item style="float: left; max-width: 350px; margin: 0;">
+          <h3 style="float: top; margin-right: 100px;">{{ product.name }} - {{ product.cost }}</h3>
+        <div style="float: bottom">
           <div style="float: left; margin-right: 15px;">
             <p>кто платил?</p>
 
             <v-radio-group v-for="(buyer, ind) in this.$store.state.buyers" :key="ind" v-model="product.buyer">
-              <v-radio  :label="buyer.name" :value="ind"></v-radio>
+              <v-radio  :label="buyer.name" :value="ind" style="margin-bottom: 19px"></v-radio>
             </v-radio-group>
 
           </div>
 
-          <div >
+          <div style="float: right">
             <p>кто ел?</p>
             <div v-for="(buyer, ind) in this.$store.state.buyers" :key="ind">
               <v-checkbox  style="margin: 0; padding: 0;" :label="buyer.name" :value="ind" v-model="product.eaters"></v-checkbox>
             </div>
-          </div>
+        </div>
         </div>
       </v-card-item>
 
@@ -46,14 +43,12 @@
   </div>
 
 
-  <v-text-field @blur="this.$store.state.tempProduct.name = $event.target.value" v-model="this.$store.state.tempProduct.name">
+  <v-text-field label="Введите название товара" @blur="this.$store.state.tempProduct.name = $event.target.value" v-model="this.$store.state.tempProduct.name">
   </v-text-field>
-  <v-text-field @blur="this.$store.state.tempProduct.cost = $event.target.value" v-model="this.$store.state.tempProduct.cost">
+  <v-text-field label="Введите стоимость товара" @blur="this.$store.state.tempProduct.cost = $event.target.value" v-model="this.$store.state.tempProduct.cost">
   </v-text-field>
   <v-btn @click="this.$store.commit('addProduct')" style="width: 100%;" >+</v-btn>
 
-
-  {{ this.$store.state.products}}
 
 
 </template>
@@ -90,7 +85,6 @@ export default {
       }
     },
     getBuyerName(){
-      //return 'xui'
       console.log(this.$store.state.buyers[1].name)
       return String(this.$store.state.buyers[1].name)
     }
